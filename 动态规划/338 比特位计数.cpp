0178ba3c504a5940e866ee952493ff010fe851e6
DamonCZH <2535180928@qq.com>
2019-11-11 11:39:32 +0800

    Update 338 比特位计数.cpp

--- 动态规划/338 比特位计数.cpp
+++ 动态规划/338 比特位计数.cpp
@@ -18,5 +18,26 @@ public:
         numOfOne(x/2,count);
     }
 };
-
 //80 ms	10 MB
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> result;
+        result.push_back(0);if(num==0) return result;
+        result.push_back(1);if(num==1) return result;
+        
+        int power_now=(int)pow(2,1),power_last=(int)pow(2,0);
+        for(int i=2;i<=num;i++){
+            if(i==power_now){
+                result.push_back(1);
+                power_last=power_now;
+                power_now*=2;
+            }
+            else
+                result.push_back(1+result[i%power_last]);
+        }
+        return result;
+    }
+};
+//72 ms	10.1 MB
