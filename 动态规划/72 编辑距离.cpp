fe85d0b94c8dcbdcf0fb72da731bcf479abaf9dc
DamonCZH <2535180928@qq.com>
2019-11-29 18:32:52 +0800

    Create 72 编辑距离.cpp

---
+++ 动态规划/72 编辑距离.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m=word1.length(), n=word2.length();
+        int dp[m+1][n+1];
+        
+        for(int i=0;i<=m;i++)
+            dp[i][0]=i;
+        for(int j=0;j<=n;j++)
+            dp[0][j]=j;
+        
+        for(int i=1;i<=m;i++)
+            for(int j=1;j<=n;j++)
+                 dp[i][j]=min(min(dp[i-1][j]+1,dp[i][j-1]+1),dp[i-1][j-1]+(word1[i-1]!=word2[j-1]));
+        
+        return dp[m][n];
+    }
+};
+//16 ms	9.5 MB
